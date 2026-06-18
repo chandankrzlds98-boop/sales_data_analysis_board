89d8094f2bdd4c91ee58bb40ab20a198ee91889f
Chandan Kumar <chandanjawan@gmail.com>
2026-06-18 15:46:31 +0530

    added message comment in main code.

--- my_first_app.py
+++ my_first_app.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from scipy import stats
-
+# Anil and Chandan work in progress
 # --- Streamlit Page Setup ---
 st.set_page_config(
     page_title="📊 Generic Data Analysis Dashboard",
